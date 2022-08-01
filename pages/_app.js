c7638db2e7046dee66c2b07d9b7327bd29b9591d
personal <personal@Abhinavs-MacBook-Air.local>
2022-08-01 17:37:11 +0530

    next error

--- pages/_app.js
+++ pages/_app.js
@@ -7,8 +7,9 @@ function MyApp({ Component, pageProps }) {
       <Script
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=G-GZTPXHWFZQ`}
+        id="ga-script-1"
       />
-      <Script strategy="lazyOnload">
+      <Script strategy="lazyOnload" id="ga-script-2">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
